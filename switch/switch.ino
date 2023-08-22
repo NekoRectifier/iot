632f69f01402ef59b03b066aa44564f311c6812f
NekoRectifier <hyx123250@gmail.com>
2023-08-22 21:49:02 +0800

    feat: add switch

---
+++ switch/switch.ino
@@ -0,0 +1,56 @@
+void setup()
+{
+    Serial.begin(115200);
+
+}
+
+void loop()
+{
+    delay(10);
+}
+
+
+//==============================
+// HomeKit setup and loop
+//==============================
+
+// access your HomeKit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_switch_on;
+
+//Called when the switch value is changed by iOS Home APP
+void cha_switch_on_setter(const homekit_value_t value) {
+	bool on = value.bool_value;
+	cha_switch_on.value.bool_value = on;	//sync the value
+	digitalWrite(PIN_SWITCH, on ? LOW : HIGH);
+}
+
+void my_homekit_setup() {
+	pinMode(PIN_SWITCH, OUTPUT);
+	digitalWrite(PIN_SWITCH, HIGH);
+
+	//Add the .setter function to get the switch-event sent from iOS Home APP.
+	//The .setter should be added before arduino_homekit_setup.
+	//HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.
+	//Maybe this is a legacy design issue in the original esp-homekit library,
+	//and I have no reason to modify this "feature".
+	cha_switch_on.setter = cha_switch_on_setter;
+	arduino_homekit_setup(&config);
+
+	//report the switch value to HomeKit if it is changed (e.g. by a physical button)
+	//bool switch_is_on = true/false;
+	//cha_switch_on.value.bool_value = switch_is_on;
+	//homekit_characteristic_notify(&cha_switch_on, cha_switch_on.value);
+}
+
+void my_homekit_loop() {
+	arduino_homekit_loop();
+	const uint32_t t = millis();
+	if (t > next_heap_millis) {
+		// show heap info every 5 seconds
+		next_heap_millis = t + 5 * 1000;
+		LOG_D("Free heap: %d, HomeKit clients: %d",
+				ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+	}
+}
